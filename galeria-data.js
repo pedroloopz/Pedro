b329b2ce1241399d7d417df854e160b49df8e9c4
pedroloopz <pedromartinslopes@proton.me>
2026-07-14 21:01:28 -0300

    Update galeria-data.js

--- galeria-data.js
+++ galeria-data.js
@@ -14,5 +14,11 @@ const GALERIA = [
   { arquivo: "assets/galeria/8.jpg", legenda: "" },
   { arquivo: "assets/galeria/9.jpg", legenda: "" },
   { arquivo: "assets/galeria/10.jpg", legenda: "" },
-  { arquivo: "assets/galeria/IMG001.jpg", legenda: "" }
+  { arquivo: "assets/galeria/IMG001.jpg", legenda: "" },
+  { arquivo: "assets/galeria/IMG-20250928-WA0028.jpg", legenda: "" },
+  { arquivo: "assets/galeria/Screenshot_20260714_204846_Gallery.jpg", legenda: "" },
+  { arquivo: "assets/galeria/Screenshot_20260714_204910_Gallery.jpg", legenda: "" },
+  { arquivo: "assets/galeria/Screenshot_20260714_205024_Gallery.jpg", legenda: "" },
+  { arquivo: "assets/galeria/Screenshot_20260714_205902_Photos.jpg", legenda: "" },
+  { arquivo: "assets/galeria/Screenshot_20260714_205918_Photos.jpg", legenda: "" }
 ];
